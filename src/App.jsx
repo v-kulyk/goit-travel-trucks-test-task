68cbb209cb7c1aca4d916df1ff0ffb75402dfea0
Volodymyr Kulyk <vlad.dev.zp@gmail.com>
2024-09-10 17:51:25 +0300

    set up basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<div>Home</div>}></Route>
+        <Route path="/catalog" element={<div>Catalog page</div>}></Route>
+        <Route
+          path="/catalog/:camperId"
+          element={<div>Camper page</div>}
+        ></Route>
+        <Route path="*" element={<div>Page not found</div>}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;

0c199b1a1811a483009f3ca8285776a45dff8c78
Volodymyr Kulyk <vlad.dev.zp@gmail.com>
2024-09-10 18:00:54 +0300

    set up page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home/Home";
+import Catalog from "./pages/Catalog/Catalog";
+import Camper from "./pages/Camper/Camper";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <>
+      <Navigation />
       <Routes>
-        <Route path="/" element={<div>Home</div>}></Route>
-        <Route path="/catalog" element={<div>Catalog page</div>}></Route>
-        <Route
-          path="/catalog/:camperId"
-          element={<div>Camper page</div>}
-        ></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/catalog" element={<Catalog />}></Route>
+        <Route path="/catalog/:camperId" element={<Camper />}></Route>
         <Route path="*" element={<div>Page not found</div>}></Route>
       </Routes>
     </>
---
+++ src/pages/Camper/Camper.jsx
@@ -0,0 +1,3 @@
+export default function Camper() {
+  return <div>Camper</div>;
+}

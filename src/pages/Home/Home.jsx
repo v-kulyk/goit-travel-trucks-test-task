d7eed730173182709295dca04d5144173b06182e
Volodymyr Kulyk <vlad.dev.zp@gmail.com>
2024-09-16 02:14:46 +0300

    implement home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,18 @@
+import styles from "./Home.module.css";
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className={styles.container}>
+        <div>
+          <h1 className={styles.heading}>Campers of your dreams</h1>
+          <p className={styles.subheading}>
+            You can find everything you want in our catalog
+          </p>
+          <a href="/catalog" className="button">
+            View Now
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }

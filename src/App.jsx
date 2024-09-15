d06928f8926bcdeccd3e6e116b573e5b4c31275d
Volodymyr Kulyk <vlad.dev.zp@gmail.com>
2024-09-16 02:10:34 +0300

    implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
+import "modern-normalize";
+import Header from "./components/Header/Header";
+import Navigation from "./components/Navigation/Navigation";
 import Home from "./pages/Home/Home";
 import Catalog from "./pages/Catalog/Catalog";
 import Camper from "./pages/Camper/Camper";
-import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <>
-      <Navigation />
+      <Header>
+        <Navigation />
+      </Header>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/catalog" element={<Catalog />}></Route>
-        <Route path="/catalog/:camperId" element={<Camper />}></Route>
+        <Route path="/catalog/:camperId" element={<Camper />}>
+          <Route
+            path="features"
+            element={<div>Features will be shown here</div>}
+          ></Route>
+          <Route
+            path="reviews"
+            element={<div>Reviews will be shown here</div>}
+          ></Route>
+        </Route>
         <Route path="*" element={<div>Page not found</div>}></Route>
       </Routes>
     </>
